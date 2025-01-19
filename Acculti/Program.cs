a9106ac19be9e8e916d1b0544d74b2ae5ef53911
TeymurAsk <teymurasgar@gmail.com>
2025-01-19 10:51:00 +0100

    starting

--- Acculti/Program.cs
+++ Acculti/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿string clientId = "1329846684594733106";
+string redirectUri = "http://localhost/acculti-auth";
+string scope = "identify";
+string authUrl = $"https://discord.com/api/oauth2/authorize?client_id={clientId}&redirect_uri={Uri.EscapeDataString(redirectUri)}&response_type=code&scope={scope}";
+Console.WriteLine("Open the following URL in your browser and authenticate:");
+Console.WriteLine(authUrl);
