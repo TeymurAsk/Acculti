f6f3eec7da5baed7607f1b181a236a024d899b88
TeymurAsk <teymurasgar@gmail.com>
2025-01-19 11:14:21 +0100

    CRUD for accounts

--- Acculti/Program.cs
+++ Acculti/Program.cs
@@ -1,4 +1,6 @@
-﻿using Newtonsoft.Json;
+﻿using Acculti;
+using Newtonsoft.Json;
+using System.Text.Json;
 
 string clientId = "1329846684594733106";
 string redirectUri = "http://localhost/acculti-auth";
@@ -21,3 +23,51 @@ string responseString = await response.Content.ReadAsStringAsync();
 var tokenData = JsonConvert.DeserializeObject<Dictionary<string, string>>(responseString);
 string accessToken = tokenData["access_token"];
 string refreshToken = tokenData["refresh_token"];
+string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Acculti", "user_accounts.json");
+void SaveAccounts(UserAccountStore accountStore)
+{
+    string directory = Path.GetDirectoryName(filePath);
+    if (!Directory.Exists(directory))
+        Directory.CreateDirectory(directory);
+
+    string json = Newtonsoft.Json.JsonSerializer.Serialize(accountStore, new JsonSerializerOptions { WriteIndented = true });
+    File.WriteAllText(filePath, json);
+}
+UserAccountStore LoadAccounts()
+{
+    if (!File.Exists(filePath))
+        return new UserAccountStore();
+
+    string json = File.ReadAllText(filePath);
+    return Newtonsoft.Json.JsonSerializer.Deserialize<UserAccountStore>(json) ?? new UserAccountStore();
+}
+void AddAccount(UserAccountStore accountStore, UserAccount account)
+{
+    accountStore.Accounts.Add(account);
+    SaveAccounts(accountStore);
+}
+void RemoveAccount(UserAccountStore accountStore, string nickname)
+{
+    var account = accountStore.Accounts.FirstOrDefault(a => a.Nickname == nickname);
+    if (account != null)
+    {
+        accountStore.Accounts.Remove(account);
+        SaveAccounts(accountStore);
+    }
+    else
+    {
+        Console.WriteLine($"Account with nickname '{nickname}' not found.");
+    }
+}
+void UpdateAccount(UserAccountStore accountStore, string nickname, UserAccount updatedAccount)
+{
+    var account = accountStore.Accounts.FirstOrDefault(a => a.Nickname == nickname);
+    if (account != null)
+    {
+        account.AccessToken = updatedAccount.AccessToken;
+        account.RefreshToken = updatedAccount.RefreshToken;
+        account.TokenExpiry = updatedAccount.TokenExpiry;
+        SaveAccounts(accountStore);
+    }
+}
+
---
+++ Acculti/UserAccountStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acculti
+{
+    public class UserAccountStore
+    {
+        public List<UserAccount> Accounts { get; set; } = new List<UserAccount>();
+    }
+}
